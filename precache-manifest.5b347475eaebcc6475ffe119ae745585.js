03798aeb37b02b1478550fb414b6474633dc2b3b
padoctb <yeahndima@gmail.com>
2020-07-27 03:48:49 +0300

    Updates

---
+++ precache-manifest.5b347475eaebcc6475ffe119ae745585.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ece29c1b7beb14c9e7bdc61eee91d95",
+    "url": "/trashauction/index.html"
+  },
+  {
+    "revision": "2b14ff2de2933909bc0e",
+    "url": "/trashauction/static/css/main.6c2c89d8.chunk.css"
+  },
+  {
+    "revision": "cf2e9577a8b08e4275a7",
+    "url": "/trashauction/static/js/2.9bc05d60.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trashauction/static/js/2.9bc05d60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b14ff2de2933909bc0e",
+    "url": "/trashauction/static/js/main.c2933a7f.chunk.js"
+  },
+  {
+    "revision": "f093b5cf523383e6b570",
+    "url": "/trashauction/static/js/runtime-main.8a9b7d79.js"
+  },
+  {
+    "revision": "eb10c48523d2acf09f8670d49fcbc852",
+    "url": "/trashauction/static/media/bg.eb10c485.jpg"
+  },
+  {
+    "revision": "eedf38f8957791d635bb5f6a1ebb875a",
+    "url": "/trashauction/static/media/delete.eedf38f8.svg"
+  },
+  {
+    "revision": "97b6b1740da29fb09a7e949b6966333d",
+    "url": "/trashauction/static/media/doubleup.97b6b174.svg"
+  },
+  {
+    "revision": "db1c444232f6edcc8439d5c5edeb4a49",
+    "url": "/trashauction/static/media/pause.db1c4442.svg"
+  },
+  {
+    "revision": "0ad03614cf9f524388e8d0a52dea868d",
+    "url": "/trashauction/static/media/play.0ad03614.svg"
+  },
+  {
+    "revision": "a081961b7d00da310bf476915ce7841c",
+    "url": "/trashauction/static/media/stop.a081961b.svg"
+  },
+  {
+    "revision": "c1bf77a40c2bde6730f6bf78036bbd37",
+    "url": "/trashauction/static/media/ticker.c1bf77a4.svg"
+  },
+  {
+    "revision": "9ed745927977cbc44e32cdd5df737c2c",
+    "url": "/trashauction/static/media/timedown.9ed74592.svg"
+  },
+  {
+    "revision": "a00f01e7290f574cf2a0283cceabf072",
+    "url": "/trashauction/static/media/timeup.a00f01e7.svg"
+  }
+]);
