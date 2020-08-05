f22e73e639559866c637b112564fd5d652e211c8
padoctb <yeahndima@gmail.com>
2020-08-05 04:16:56 +0300

    removed sourcemaps for prod build

--- src/App.js
+++ src/App.js
@@ -124,6 +124,8 @@ const App = () => {
   const [bgInputValue, setBgInputValue] = useState(bgFromLs);
 
   useEffect(() => {
+    console.log('%cЧувак, ты думал что-то здесь будет? О, нет.', "color: #fff; font-size: 26px;")
+    console.log('%cИсходнный код: https://github.com/zhukosvinka/trashauction/tree/master', "color: #fff; font-size: 26px;")
     document.addEventListener('click', (e) => {
       const isClickedOut =
         e.path.find((element) => element.id === 'settings-wrapper') === undefined;
